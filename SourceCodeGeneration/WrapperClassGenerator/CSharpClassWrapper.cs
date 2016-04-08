3e4456767a82de2a68e503d5837e64ad40d97cfa
Peter Csikos <pcsikos@gmail.com>
2016-04-08 13:51:17 +0200

    Excluding methods from wrapper class generation

--- SourceCodeGeneration/WrapperClassGenerator/CSharpClassWrapper.cs
+++ SourceCodeGeneration/WrapperClassGenerator/CSharpClassWrapper.cs
@@ -1,5 +1,7 @@
 ﻿
 using WrapperClassGenerator;
+using System;
+using System.Collections.Generic;
 
 namespace WrapperClassGenerator
 {
@@ -12,10 +14,6 @@ namespace WrapperClassGenerator
 			_sampleclass = sampleclass;
 		}
 
-		public virtual void Method0<TValue>() 		
-		{
-			_sampleclass.Method0<TValue>();
-		}
 		public virtual void Method1<TValue>() 
             where TValue: class		
 		{
@@ -27,7 +25,7 @@ namespace WrapperClassGenerator
 			_sampleclass.Method2<TValue>();
 		}
 		public virtual void Method3<TValue>() 
-            where TValue: System.IComparable		
+            where TValue: IComparable		
 		{
 			_sampleclass.Method3<TValue>();
 		}
@@ -37,21 +35,21 @@ namespace WrapperClassGenerator
 		{
 			_sampleclass.Method4<TValue, TResult>();
 		}
-		public virtual void Method5<TValue>(System.Func<TValue> func) 
-            where TValue: System.IComparable		
+		public virtual void Method5<TValue>(Func<TValue> func) 
+            where TValue: IComparable		
 		{
 			_sampleclass.Method5<TValue>(func);
 		}
-		public virtual System.Collections.Generic.IEnumerable<TValue> Method6<TValue>(System.Collections.Generic.IEnumerable<TValue> func) 
-            where TValue: System.IComparable		
+		public virtual IEnumerable<TValue> Method6<TValue>(IEnumerable<TValue> func) 
+            where TValue: IComparable		
 		{
 			return _sampleclass.Method6<TValue>(func);
 		}
-		public virtual void Method7(string str, params System.Type[] types) 		
+		public virtual void Method7(string str, params Type[] types) 		
 		{
 			_sampleclass.Method7(str, types);
 		}
-		public virtual long Method8(ref System.Type[] types) 		
+		public virtual long Method8(ref Type[] types) 		
 		{
 			return _sampleclass.Method8(ref types);
 		}
@@ -67,8 +65,6 @@ namespace WrapperClassGenerator
 	public partial interface ISampleClass		
 	{
 
-      void Method0<TValue>();
-
       void Method1<TValue>()
             where TValue: class;
 
@@ -76,21 +72,21 @@ namespace WrapperClassGenerator
             where TValue: struct;
 
       void Method3<TValue>()
-            where TValue: System.IComparable;
+            where TValue: IComparable;
 
       void Method4<TValue, TResult>()
             where TValue: class
             where TResult: TValue;
 
-      void Method5<TValue>(System.Func<TValue> func)
-            where TValue: System.IComparable;
+      void Method5<TValue>(Func<TValue> func)
+            where TValue: IComparable;
 
-      System.Collections.Generic.IEnumerable<TValue> Method6<TValue>(System.Collections.Generic.IEnumerable<TValue> func)
-            where TValue: System.IComparable;
+      IEnumerable<TValue> Method6<TValue>(IEnumerable<TValue> func)
+            where TValue: IComparable;
 
-      void Method7(string str, params System.Type[] types);
+      void Method7(string str, params Type[] types);
 
-      long Method8(ref System.Type[] types);
+      long Method8(ref Type[] types);
 
       string Method9(dynamic input);
 
